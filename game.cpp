0d92abbb2fe783032dec0b5a95f0f544aab35109
Matteo Paoli <matteo_paoli@outlook.com>
2023-08-14 00:20:32 +0800

    feat: introducing attempts

--- game.cpp
+++ game.cpp
@@ -1,7 +1,7 @@
 /*
   Author: Matteo Paoli
   Program: Guessing the number
-  Version: 1.0
+  Version: 1.1
   
   Description:
   A sample game I've created for learning C++
@@ -25,25 +25,32 @@ int main() {
   std::string userInput;
   bool guessed = false;
   int randomNumber = std::rand() % 10 + 1;
+  int attempts = 5;
   
-  while (!guessed) {
-    std::cout << "Guess the number: ";
-    std::getline(std::cin, userInput);
-    int userGuess;
-    try {
-      userGuess = std::stoi(userInput);
-    }
-    catch (const std::invalid_argument& e) {
-      std::cout << "Invalid input: " << e.what() << std::endl;
-      continue;
-    }
-    if (userGuess == randomNumber) {
-      guessed = true;
-      std::cout << "You guessed! The number was " << randomNumber << std::endl;
-    }
-    else {
-      std::cout << "Nice try, but that's not the right number." << std::endl;
-    }
+  for (int i = attempts; i > 0; i--) {
+      int userGuess;
+      std::cout << std::to_string(i) << " attempts left" << std::endl;
+      std::cout << "Guess the number: ";
+      std::getline(std::cin, userInput);
+      try {
+        userGuess = std::stoi(userInput);
+      }
+      catch (const std::invalid_argument& e) {
+        std::cout << "Invalid input: " << e.what() << std::endl;
+        i++;
+        continue;
+      }
+      if (userGuess == randomNumber) {
+        std::cout << "You guessed! The number was " << randomNumber << std::endl;
+        guessed = true;
+        break;
+      }
+      else {
+        std::cout << "Nice try, but that's not the right number." << std::endl;
+      }
+  }
+  if (!guessed) {
+    std::cout << "You lost the game, try again." << std::endl;
   }
   return 0;
 }
