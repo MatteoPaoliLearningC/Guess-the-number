e69ab0b1f77d88966ac56b8e714352de860b0ed3
Matteo Paoli <matteo_paoli@outlook.com>
2023-08-14 17:41:02 +0800

    refactor: move game iteration into separate function

--- game.cpp
+++ game.cpp
@@ -1,7 +1,7 @@
 /*
   Author: Matteo Paoli
   Program: Guessing the number
-  Version: 1.2
+  Version: 1.2.1
 
   Description:
   A sample game I've created for learning C++
@@ -41,6 +41,19 @@ std::map<std::string, int> get_settings() {
   }
 }
 
+bool game_iteration(int randomNumber) {
+  int userGuess;
+  std::string userInput;
+  std::cout << "Guess the number: ";
+  std::getline(std::cin, userInput);
+  try {
+    userGuess = std::stoi(userInput);
+  } catch (const std::invalid_argument &e) {
+    throw std::runtime_error("Failed to parse user input");
+  }
+  return userGuess == randomNumber;
+}
+
 int main() {
   std::map<std::string, int> settings;
   try {
@@ -50,31 +63,27 @@ int main() {
     return 1;
   }
   std::srand(std::time(0));
-  std::string userInput;
+
   bool guessed = false;
   int randomNumber = std::rand() % settings["maxRange"] + 1;
 
   for (int i = settings["attempts"]; i > 0; i--) {
-    int userGuess;
     std::cout << std::to_string(i) << " attempts left" << std::endl;
-    std::cout << "Guess the number: ";
-    std::getline(std::cin, userInput);
     try {
-      userGuess = std::stoi(userInput);
-    } catch (const std::invalid_argument &e) {
+      if (game_iteration(randomNumber)) {
+        guessed = true;
+        break;
+      } else {
+        std::cout << "Nice try, but that's not the right number." << std::endl;
+      }
+    } catch (const std::exception &e) {
       std::cout << "Invalid input: " << e.what() << std::endl;
       i++;
-      continue;
-    }
-    if (userGuess == randomNumber) {
-      std::cout << "You guessed! The number was " << randomNumber << std::endl;
-      guessed = true;
-      break;
-    } else {
-      std::cout << "Nice try, but that's not the right number." << std::endl;
     }
   }
-  if (!guessed) {
+  if (guessed) {
+    std::cout << "You guessed! The number was " << randomNumber << std::endl;
+  } else {
     std::cout << "The number was " << randomNumber << "." << std::endl;
     std::cout << "You lost the game, try again." << std::endl;
   }
