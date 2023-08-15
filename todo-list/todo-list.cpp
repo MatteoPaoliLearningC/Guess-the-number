ad1a04e1a84300bd9d82a21309e16a41d4d5a8ab
Matteo Paoli <matteo_paoli@outlook.com>
2023-08-15 15:22:21 +0800

    chore: create todo_list project

---
+++ todo-list/todo-list.cpp
@@ -0,0 +1,22 @@
+/*
+  Author: Matteo Paoli
+  Program: Todo list
+  Version: 0.1.0
+
+  Description:
+  My personal implementation of the most common programming exercise ever.
+
+  Usage:
+  [Instructions on how to use the program.]
+
+  Notes:
+  [Any additional notes or information about the program.]
+
+  Date: 15/08/2023 - last review 15/08/2023
+*/
+#include <iostream>
+
+int main() {
+  std::cout << "TODO LIST" << std::endl;
+  return 0;
+}
