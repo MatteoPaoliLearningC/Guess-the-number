544d1ffc5a6aae44a8e15f90fe2263891c8604ca
Matteo Paoli <matteo_paoli@outlook.com>
2023-08-22 23:39:24 +0800

    feat(todo_list): migrate to OOP

--- todo-list/todo-list.cpp
+++ todo-list/todo-list.cpp
@@ -1,7 +1,7 @@
 /*
   Author: Matteo Paoli
   Program: Todo list
-  Version: 1.1.0
+  Version: 2.0.0
 
   Description:
   My personal implementation of the most common programming exercise ever.
@@ -14,13 +14,72 @@
 
   Date: 15/08/2023 - last review 22/08/2023
 */
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
 
-const std::string SW_VERSION = "1.1.0";
+const std::string SW_VERSION = "2.0.0";
 const std::string SAVE_FILE_NAME = "todo.txt";
+
+class TodoList {
+ public:
+  TodoList(const std::string &file_name): file_name(file_name) {
+    std::ifstream todoFile(file_name);
+    if (todoFile.is_open()) {
+      std::string line;
+      while (std::getline(todoFile, line)) {
+        tasks.push_back(line);
+      }
+      todoFile.close();
+    } else {
+      std::cout << "Save file not found, creating..." << std::endl;
+      std::ofstream outfile(file_name);
+    }
+  }
+
+  void add(const std::string &task) {
+    tasks.push_back(task);
+    save();
+  }
+
+  void edit(size_t index, const std::string &task) {
+    if (index >= 0 && index < tasks.size()) {
+      tasks[index] = task;
+    }
+    save();
+  }
+
+  void remove(size_t index) {
+    if (index >= 0 && index < tasks.size()) {
+      tasks.erase(tasks.begin() + index);
+    }
+    save();
+  }
+
+  void print() {
+    for (size_t i = 0; i < tasks.size(); i++) {
+      std::cout << i + 1 << ") " << tasks[i] << std::endl;
+    }
+  }
+
+  bool isEmpty() { return tasks.empty(); }
+
+ private:
+  std::vector<std::string> tasks;
+  std::string file_name; 
+
+  void save() {
+    std::ofstream outfile(file_name);
+    if (outfile.is_open()) {
+      for (const std::string &item : tasks) {
+        outfile << item << std::endl;
+      }
+    } else {
+      std::cerr << "Error opening file: " << file_name << std::endl;
+    }
+  }
+};
 
 std::string getUserInput() {
   std::string userInput;
@@ -28,100 +87,44 @@ std::string getUserInput() {
   return userInput;
 }
 
-std::vector<std::string> readSave() {
-  std::vector<std::string> list;
-  std::ifstream todoFile(SAVE_FILE_NAME);
-  if (todoFile.is_open()) {
-    std::string line;
-    while (std::getline(todoFile, line)) {
-      list.push_back(line);
-    }
-  }
-  else {
-    std::cout << "Save file not found, creating..." << std::endl;
-    std::ofstream outfile(SAVE_FILE_NAME);
-  }
-  return list;
-}
-
-void save(const std::vector<std::string> &todoList) {
-  std::ofstream outfile(SAVE_FILE_NAME);
-  if (outfile.is_open()) {
-    for (const std::string &item : todoList) {
-      outfile << item << std::endl;
-    }
-  }
-  else {
-    std::cerr << "Error opening file: " << SAVE_FILE_NAME << std::endl;
-  }
-}
-
-void printToDoTasks(const std::vector<std::string> &todoList) {
-  for (size_t i = 0; i < todoList.size(); i++) {
-    std::cout << i + 1 << ") " << todoList[i] << std::endl;
-  }
-}
-
-void addTask(std::vector<std::string> &todoList) {
-  std::string userInput;
-  std::cout << "Enter your activity: ";
-  todoList.push_back(getUserInput());
-  save(todoList);
-}
-
-void deleteTask(std::vector<std::string> &todoList) {
-  if (todoList.empty()) {
-    std::cout << "You have no items in your list." << std::endl;
-    return;
-  }
-  printToDoTasks(todoList);
-  std::cout << "Which one do you want to delete? ";
-  size_t indexToRemove = std::stoul(getUserInput()) - 1;
-  if (indexToRemove >= 0 && indexToRemove < todoList.size()) {
-    todoList.erase(todoList.begin() + indexToRemove);
-    save(todoList);
-  } else {
-    std::cout << "Invalid index." << std::endl;
-  }
-}
-
-void editTask(std::vector<std::string> &todoList) {
-  if (todoList.empty()) {
-    std::cout << "You have no items in your list." << std::endl;
-    return;
-  }
-  printToDoTasks(todoList);
-  std::cout << "Which one do you want to edit? ";
-  size_t indexToEdit = std::stoul(getUserInput()) - 1;
-  if (indexToEdit >= 0 && indexToEdit < todoList.size()) {
-    std::cout << "Edit the item" << std::endl;
-    todoList[indexToEdit] = getUserInput();
-    save(todoList);
-  }
-}
-
 int main() {
   std::string userInput;
-  std::vector<std::string> todoList = readSave();
+  TodoList todoList(SAVE_FILE_NAME);
   std::cout << "TO-DO LIST" << std::endl;
   std::cout << "Developed by Matteo Paoli - v" << SW_VERSION << std::endl;
   std::cout << "\n";
 
   while (true) {
-    if (todoList.empty()) {
+    if (todoList.isEmpty()) {
       std::cout << "Your to-do list is empty" << std::endl;
     } else {
       std::cout << "Here is your current to-do list" << std::endl;
-      printToDoTasks(todoList);
+      todoList.print();
     }
     std::cout << "What do you wanna do? (add / delete / edit)" << std::endl;
     userInput = getUserInput();
     if (userInput == "add") {
-      addTask(todoList);
+      std::cout << "Enter your activity: ";
+      todoList.add(getUserInput());
     } else if (userInput == "delete") {
-      deleteTask(todoList);
+      if (todoList.isEmpty()) {
+        std::cout << "You have no items in your list." << std::endl;
+      } else {
+        todoList.print();
+        std::cout << "Which one do you want to delete? ";
+        size_t indexToRemove = std::stoul(getUserInput()) - 1;
+        todoList.remove(indexToRemove);
+      }
     } else if (userInput == "edit") {
-      editTask(todoList);
+      if (todoList.isEmpty()) {
+        std::cout << "You have no items in your list." << std::endl;
+      } else {
+        todoList.print();
+        std::cout << "Which one do you want to edit? ";
+        size_t indexToEdit = std::stoul(getUserInput()) - 1;
+        std::cout << "Edit the item" << std::endl;
+        todoList.edit(indexToEdit, getUserInput()); 
+      }
     } else {
       std::cout << "Invalid input" << std::endl;
     }
